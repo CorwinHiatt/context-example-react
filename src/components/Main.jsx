524b478eb675ff8b6b788b2d0a3c9ba0acec4252
Corwin Hiatt <corwinhiatt@yahoo.com>
2022-11-03 11:08:20 -0400

    ready to use context

---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import Welcome from "./Wlecome"
+import Footer from "./Footer"
+
+export default function Main() {
+    return(
+        <main>
+            <Welcome />
+          <Footer />
+        </main>
+    )
+}
